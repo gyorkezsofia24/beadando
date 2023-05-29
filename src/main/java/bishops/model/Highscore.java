36cdb1ab91a51a04828684c6aa66c49924d575bf
gyorkezsofia24 <gyorkezsofia.8@gmail.com>
2023-05-30 00:52:29 +0200

    Highscore.java

---
+++ src/main/java/bishops/model/Highscore.java
@@ -0,0 +1,22 @@
+package bishops.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+public class Highscore {
+
+    private StringProperty name = new SimpleStringProperty("");
+
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+}
+
